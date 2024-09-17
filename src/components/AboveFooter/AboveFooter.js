e3da22b42873808f3b62f49623794860e36b5320
KyerrreN <kyerrrenmgt@gmail.com>
2024-09-17 23:43:04 +0300

    feat: add props to get in touch (above footer) section

--- src/components/AboveFooter/AboveFooter.js
+++ src/components/AboveFooter/AboveFooter.js
@@ -3,17 +3,18 @@ import "../AboveFooter/AboveFooter.css";
 
 class AboveFooter extends Component {
     render() {
+        const { header, desc } = this.props;
+
         return (
             <div className="container">
                 <div id="above-footer-frame" className="to-change-frame">
                     <div className="above-footer-content">
                         <div className="above-footer-content-text">
                             <span className="above-footer-content-text-header to-change-text">
-                                Get in touch
+                                {header}
                             </span>
                             <span className="above-footer-content-text-desc to-change-text">
-                                Think we would be a good fit for your next
-                                project?
+                                {desc}
                             </span>
                         </div>
 
